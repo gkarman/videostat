5a18fbc608604200ef0dba12ad0bcb57a1010195
Григорий Карман <g.karman@mail.ru>
2026-04-06 09:34:39 +0300

    настройка телеграмм

--- internal/infrastructure/transport/telegram/bot.go
+++ internal/infrastructure/transport/telegram/bot.go
@@ -22,6 +22,11 @@ type Bot struct {
 	done         chan struct{}
 }
 
+var defaultCommands = []tgbotapi.BotCommand{
+	{Command: "start", Description: "Запуск бота"},
+	{Command: "help", Description: "Список команд"},
+}
+
 func NewBot(cfg *Config, log *slog.Logger) (*Bot, error) {
 	botAPI, err := tgbotapi.NewBotAPI(cfg.Token)
 	if err != nil {
@@ -29,7 +34,6 @@ func NewBot(cfg *Config, log *slog.Logger) (*Bot, error) {
 	}
 
 	botAPI.Debug = cfg.Debug
-	log.Info("telegram authorized on account %s", botAPI.Self.UserName)
 
 	updateConfig := tgbotapi.NewUpdate(0)
 	updateConfig.Timeout = cfg.Timeout
@@ -47,7 +51,9 @@ func (b *Bot) Start(ctx context.Context) {
 	b.log.Info("start telegram bot")
 	go func() {
 		defer close(b.done)
-
+		if err := b.setCommands(); err != nil {
+			b.log.Error("set commands", "err", err)
+		}
 		updates := b.botAPI.GetUpdatesChan(b.updateConfig)
 
 		for {
@@ -73,16 +79,84 @@ func (b *Bot) Start(ctx context.Context) {
 }
 
 func (b *Bot) handle(update tgbotapi.Update) {
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID,
-		"Ты написал: "+update.Message.Text)
+	msg := update.Message
 
-	if _, err := b.botAPI.Send(msg); err != nil {
-		b.log.Error("send message", "err", err)
+	if msg.IsCommand() {
+		b.handleCommand(msg)
+		return
 	}
+
+	b.reply(msg.Chat.ID, "Я понимаю только команды. Нажми /help")
 }
 
 func (b *Bot) Stop() {
 	b.log.Info("stop telegram bot")
 	close(b.stop)
 	<-b.done
+}
+
+func (b *Bot) setCommands() error {
+	cfg := tgbotapi.NewSetMyCommands(defaultCommands...)
+	_, err := b.botAPI.Request(cfg)
+	return err
+}
+
+func (b *Bot) handleCommand(msg *tgbotapi.Message) {
+	switch msg.Command() {
+
+	case "start":
+		//msg := tgbotapi.NewMessage(msg.Chat.ID, "Привет! Выбери команду из меню ниже:")
+		//msg.ReplyMarkup = b.startKeyboard()
+		//msg.ParseMode = tgbotapi.ModeHTML
+		//if _, err := b.botAPI.Send(msg); err != nil {
+		//	b.log.Error("send message", "err", err)
+		//}
+		msg := tgbotapi.NewMessage(msg.Chat.ID, "Привет! Выбери команду:")
+		msg.ReplyMarkup = b.startInlineKeyboard()
+		b.botAPI.Send(msg)
+
+	case "help":
+		b.reply(msg.Chat.ID, b.commandsText())
+
+
+	default:
+		b.reply(msg.Chat.ID, "Неизвестная команда")
+	}
+}
+
+func (b *Bot) commandsText() string {
+	return `<b>Доступные команды</b>
+
+🚀 <code>/start</code> — запуск бота
+📖 <code>/help</code> — список команд
+
+Выбери команду из меню или введи её вручную.`
+}
+
+func (b *Bot) reply(chatID int64, text string) {
+	msg := tgbotapi.NewMessage(chatID, text)
+	msg.ParseMode = tgbotapi.ModeHTML
+
+	if _, err := b.botAPI.Send(msg); err != nil {
+		b.log.Error("send message", "err", err)
+	}
+}
+
+
+func (b *Bot) startKeyboard() tgbotapi.ReplyKeyboardMarkup {
+	return tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("/start"),
+			tgbotapi.NewKeyboardButton("/help"),
+		),
+	)
+}
+
+func (b *Bot) startInlineKeyboard() tgbotapi.InlineKeyboardMarkup {
+	return tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Start", "cmd_start"),
+			tgbotapi.NewInlineKeyboardButtonData("Help", "cmd_help"),
+		),
+	)
 }
